8b559bf8c4a09b3f22cf435564c358fc0f878776
Andres Martinez <amartinez@goldstar.com>
2024-11-26 16:41:04 -0300

    Initial Commit

---
+++ Cities/CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesApp.swift
+//  Cities
+//
+//  Created by andres martinez on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
